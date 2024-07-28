5897df770bfef20ee6c296c408b2fcc3bf131b9d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-07-27 18:07:46 -0600

    Created: active link functionality

--- components/active-link/ActiveLink.tsx
+++ components/active-link/ActiveLink.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface ActiveLinkProps {
   path: string;
@@ -6,12 +8,12 @@ interface ActiveLinkProps {
 }
 
 export default function ActiveLink({ path, text }: ActiveLinkProps) {
+  const pathName = usePathname();
   return (
     <li>
       <Link
         href={path}
-        className="block py-2 px-3 md:p-0 text-white rounded md:hover:text-blue-500 transition-colors"
-        aria-current="page"
+        className={`${path === pathName ? 'text-blue-500' : 'text-white'} block py-2 px-3 md:p-0 rounded md:hover:text-blue-500 transition-colors`}
       >
         {text}
       </Link>
